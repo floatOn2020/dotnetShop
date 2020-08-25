44477c42a1986efc707d2bf0077f0399a755e7c5
Bianca Herbst <b.herbst@posteo.de>
2020-08-25 14:33:48 +0200

    add tools

--- dotnetShop/Models/AppDbContext.cs
+++ dotnetShop/Models/AppDbContext.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore
+﻿using Microsoft.EntityFrameworkCore;
 using System;
 namespace dotnetShop.Models
 {
--- dotnetShop/Startup.cs
+++ dotnetShop/Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Configuration
+using Microsoft.Extensions.Configuration;
 
 namespace dotnetShop
 {
