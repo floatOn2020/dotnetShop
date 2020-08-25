0c93a43e2eb5c710e6278cbc6875c3d55912efc0
Bianca Herbst <b.herbst@posteo.de>
2020-08-25 11:37:53 +0200

    setup ORM

---
+++ dotnetShop/Models/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore
+using System;
+namespace dotnetShop.Models
+{
+    public class AppDbContext :DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) 
+: base(options)
+        {
+        }
+
+        public DbSet<Candy> Candies { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
--- dotnetShop/Models/CategoryRepository.cs
+++ dotnetShop/Models/CategoryRepository.cs
@@ -1,15 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace dotnetShop.Models
 {
+
     public class CategoryRepository : ICategoryRepository
     {
-        public IEnumerable<Category> GetAllCategories => new List<Category>
-        {
-            new Category{CategoryId=1, CategoryName="Licorice", CategoryDescription="Best of Denmark"},
-            new Category{CategoryId=2, CategoryName="Chocolates", CategoryDescription="Blocks of Happiness"},
-            new Category{CategoryId=3, CategoryName="Fruit Candy", CategoryDescription="Like real fruit, just without nutrition value"}
-        };
+        private readonly AppDbContext _appDbContext;
+
+        public CategoryRepository(AppDbContext appDbContext)
+        { 
+            _appDbContext = appDbContext;
+        }
+    
+        public IEnumerable<Category> GetAllCategories => _appDbContext.Categories;
+            
+        
+            //new Category{CategoryId=1, CategoryName="Licorice", CategoryDescription="Best of Denmark"},
+            //new Category{CategoryId=2, CategoryName="Chocolates", CategoryDescription="Blocks of Happiness"},
+            //new Category{CategoryId=3, CategoryName="Fruit Candy", CategoryDescription="Like real fruit, just without nutrition value"}
     }
 }
